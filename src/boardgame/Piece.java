70c3ddc271d247959f6035c70714d86f98a14a48
pereiraflausino <pereiraflausino@gmail.com>
2020-04-28 20:22:28 -0300

    Starting to implement Board and Piece

---
+++ src/boardgame/Piece.java
@@ -0,0 +1,19 @@
+package boardgame;
+
+import javax.swing.border.Border;
+
+public class Piece {
+	
+	protected Position position;
+	private Board board;
+	
+	public Piece(Board board) {
+		this.board = board;
+		position = null;
+	}
+
+	protected Board getBoard() {
+		return board;
+	}
+
+}
